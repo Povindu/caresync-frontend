2aeb646fcbcc7d32bc05115cdad702d8953853b8
Prasadini V.G.H <prasadinivgh.21@uom.lk>
2024-01-09 21:09:11 +0530

    pra-fea-start and stop button
    add start stop button and call all to stepCounterHome page

---
+++ Components/StepCounterPage/StepCount.js
@@ -0,0 +1,25 @@
+import { View, StyleSheet ,Text} from "react-native";
+
+const StepCount = () => {
+  return (
+    <View style={styles.container}>
+        <Text style={styles.texts}>Steps : </Text>
+        <Text style={styles.texts}>00</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    container:{
+        alignItems:'center',
+        justifyContent:"center",
+        paddingTop:20,
+        display:"flex",
+        flexDirection:"row",
+    },
+    texts:{
+        fontSize:26,
+    }
+});
+
+export default StepCount;
--- Components/StepCounterPage/StepCounterHome.js
+++ Components/StepCounterPage/StepCounterHome.js
@@ -1,10 +1,14 @@
 import { View } from "react-native";
 import TestHeader from "../Header";
+import StepCountButton from "./StepCountButton";
+import StepCount from "./StepCount";
 
 const StepCounterHome=()=>{
     return(
         <View>
             <TestHeader name={"Step Counter"}/>
+            <StepCount/>
+            <StepCountButton/>
         </View>
 
     );

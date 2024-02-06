ced3b741f587e958528472ea8660c131427969a6
Povindu Samarasekara <povinduchanmith@gmail.com>
2024-02-06 22:52:03 +0530

    fix: Accidential DatePicker line commented bug fixed
    DatePicker Not found error fixed

--- src/screens/AddMedicalIncidentScreen/components/Calendar.js
+++ src/screens/AddMedicalIncidentScreen/components/Calendar.js
@@ -9,7 +9,7 @@ import {
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useState } from "react";
-// import DatePicker from "react-native-modern-datepicker";
+import DatePicker from "react-native-modern-datepicker";
 import { getFormatedDate } from "react-native-modern-datepicker";
 
 function Calendar() {

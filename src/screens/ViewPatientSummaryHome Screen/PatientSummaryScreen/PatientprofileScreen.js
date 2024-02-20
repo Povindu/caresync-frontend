bb756465fcc88d3ee0e1395355e1390a37cf2014
Lakpriya D.D.S <lakpriyadds.21@uom.lk>
2024-02-20 16:59:25 +0530

    fix: add local host and port 4000

--- src/screens/ViewPatientSummaryHome Screen/PatientSummaryScreen/PatientprofileScreen.js
+++ src/screens/ViewPatientSummaryHome Screen/PatientSummaryScreen/PatientprofileScreen.js
@@ -13,7 +13,7 @@ function PatientProfileScreen({ route, navigation }) {
 
   const fetchPatients = async () => {
     try {
-      const response = await axios.get('http://192.168.235.160:3008/patients');
+      const response = await axios.get('http://localhost:4000/patients');
       setPatients(response.data);
     } catch (error) {
       console.error('Error fetching patients:', error);
--- src/screens/ViewPatientSummaryHome Screen/PatientSummaryScreen/PatientsScreen.js
+++ src/screens/ViewPatientSummaryHome Screen/PatientSummaryScreen/PatientsScreen.js
@@ -18,7 +18,7 @@ function PatientsScreen({navigation}){
 
   const fetchPatients = async () => {
     try {
-      const response = await axios.get('http://192.168.235.160:3008/patients');
+      const response = await axios.get('http://localhost:4000/patients');
       setPatients(response.data);
     } catch (error) {
       console.error('Error fetching patients:', error);

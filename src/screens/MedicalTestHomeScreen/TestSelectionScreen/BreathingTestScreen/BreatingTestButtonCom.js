27a709275610e3ad1fc183121750460ea417a510
Prasadini V.G.H <prasadinivgh.21@uom.lk>
2024-02-23 12:15:42 +0530

    API integration in breathing test

--- src/screens/MedicalTestHomeScreen/TestSelectionScreen/BreathingTestScreen/BreatingTestButtonCom.js
+++ src/screens/MedicalTestHomeScreen/TestSelectionScreen/BreathingTestScreen/BreatingTestButtonCom.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import {
   View,
   Text,
@@ -6,9 +6,11 @@ import {
   StyleSheet,
   Alert,
   ScrollView,
+  TouchableOpacity
 } from "react-native";
 import DisplayTime from "../../../../components/StopwatchDisplay";
 import BreathingTestDataStore from "./BreathingTestDataStore";
+import axios from "axios";
 
 const HoldButton = () => {
   const sDate = new Date().toDateString();
@@ -25,6 +27,47 @@ const HoldButton = () => {
     updatedM = time.m,
     updatedH = time.h;
 
+  useEffect(() => {
+    getResults();
+  }, []);
+
+  const getResults = () => {
+    axios
+      .get("http://192.168.43.192:4000/api/breathingTests")
+      .then((response) => {
+        setResult(response.data || []);
+      })
+      .catch((error) => {
+        console.error("Axios Error : ", error);
+      });
+  };
+
+  const addResults = (data) => {
+    const payload = {
+      date: data.date,
+      stopwatchTime : data.stopwatchTime,
+    }
+    axios
+      .post('http://192.168.43.192:4000/api/breathingTests', payload)
+      .then(() => {
+        getResults();
+      })
+      .catch((error) => {
+        console.error("Axios Error : ", error);
+      });
+  };
+
+  const deleteResults = ()=> {
+    axios
+      .delete('http://192.168.43.192:4000/api/breathingTests')
+      .then(() => {
+        getResults();
+      })
+      .catch((error) => {
+        console.error("Axios Error : ", error);
+      });
+  };
+
   const handlePressIn = () => {
     setIsPressing(true);
     intervalRef.current = setInterval(run, 1000);
@@ -40,14 +83,8 @@ const HoldButton = () => {
     }
   };
 
-  const padtoTwo =(number) =>(number<=9 ? `0${number}` : number);
-
-  const saveData = () => {
-    console.log(updatedH,updatedM,updatedS);
-    console.log(sDate);
-    setResult((prevResult) => [...prevResult, {date: sDate, time: `${padtoTwo(updatedH)}:${padtoTwo(updatedM)}:${padtoTwo(updatedS)}` }]);
-    resetTime();
-  };
+  const padtoTwo = (number) => (number <= 9 ? `0${number}` : number);
+  const sTime = `${padtoTwo(updatedH)}:${padtoTwo(updatedM)}:${padtoTwo(updatedS)}`;
 
   const showDecisionBox = () => {
     Alert.alert("Save Details", "Do you want to save your test result?", [
@@ -60,7 +97,8 @@ const HoldButton = () => {
       {
         text: "Save",
         onPress: () => {
-          saveData();
+          addResults({date : sDate, stopwatchTime : sTime})
+          resetTime();
         },
       },
     ]);
@@ -108,7 +146,9 @@ const HoldButton = () => {
           <BreathingTestDataStore sampleData={result} />
         </View>
         <View style={styles.resetTable}>
+        <TouchableOpacity onPress={deleteResults}>
           <Text style={{ color: "#990000" }}>Reset</Text>
+        </TouchableOpacity>
         </View>
       </View>
     </ScrollView>
@@ -155,6 +195,7 @@ const styles = StyleSheet.create({
   resetTable: {
     justifyContent: "center",
     alignItems: "center",
+    marginBottom: 20,
   },
 });
 

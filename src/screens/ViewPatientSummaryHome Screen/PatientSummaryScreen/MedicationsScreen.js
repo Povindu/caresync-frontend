14f46b87ea9721f71d268413c96ac7dcb88fc134
Lakpriya D.D.S <lakpriyadds.21@uom.lk>
2024-03-04 00:14:19 +0530

    feat: medication page setup with backend

--- src/screens/ViewPatientSummaryHome Screen/PatientSummaryScreen/MedicationsScreen.js
+++ src/screens/ViewPatientSummaryHome Screen/PatientSummaryScreen/MedicationsScreen.js
@@ -2,28 +2,44 @@ import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
-  Modal,
-  TextInput,
   Button,
+  TextInput,
+  Modal,
   StyleSheet,
-  FlatList,
+  TouchableOpacity,
 } from "react-native";
 import axios from "axios";
+import { Calendar, Agenda } from "react-native-calendars";
 import Header2 from "../../AddMedicalIncidentScreen/components/Header2";
-import { Agenda } from "react-native-calendars";
 
-const MedicationsScreen = () => {
-  const [items, setItems] = useState({});
-  const [selectedDate, setSelectedDate] = useState(null);
-  const [eventText, setEventText] = useState("");
-  const [modalVisible, setModalVisible] = useState(false);
-  const [newlyAddedEvents, setNewlyAddedEvents] = useState([]);
-  const [patientId, setPatientId] = useState(null);
-  const [medications, setMedications] = useState({});
-
+const MedicationScreen = () => {
+  const [medications, setMedications] = useState([]);
+  const [patientId, setPatientId] = useState("");
+  const [selectedDate, setSelectedDate] = useState("");
   const [medicalDetails, setMedicalDetails] = useState("");
+  const [modalVisible, setModalVisible] = useState(false);
+  const [items, setItems] = useState({});
+  const [medicationsModalVisible, setMedicationsModalVisible] = useState(false);
+  const [medicationsForSelectedDay, setMedicationsForSelectedDay] = useState(
+    []
+  );
+  const [markedDates, setMarkedDates] = useState({});
 
-  //////////////////////////////
+  // useEffect(() => {
+  //   fetchMedications();
+  // }, []);
+
+  // const fetchMedications = async () => {
+  //   try {
+  //     const response = await axios.post(
+  //       "http://192.168.8.104:8011/medications"
+  //     );
+  //     setMedications(response.data);
+  //   } catch (error) {
+  //     console.error("Error fetching medications:", error);
+  //     // Handle error
+  //   }
+  // };
 
   const formatDate = (dateString) => {
     const date = new Date(dateString);
@@ -33,14 +49,9 @@ const MedicationsScreen = () => {
 
     return `${year}-${month}-${day}`;
   };
-
   const fetchMedications = async () => {
     try {
-      // const response = await axios.get('http://192.168.75.160:8011/medications',{
-      //     patientId: '65cde7c585ffe2b8d4a75878'
-      // });
-
-      const response = await fetch("http://192.168.75.160:8011/medications", {
+      const response = await fetch("http://192.168.8.104:8011/medications", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -49,69 +60,75 @@ const MedicationsScreen = () => {
           patientId: "65cde7c585ffe2b8d4a75878", // Replace with actual patient ID
         }),
       });
-      // Assuming the response data is an array of medications
-      const responseJson = await response.json();
 
-      const newData = responseJson.map((item) => {
-        return {
-          selectedDate: formatDate(item.selectedDate),
-          medicalDetails: item.medicalDetails,
+      if (!response.ok) {
+        throw new Error("Failed to fetch medications");
+      }
+
+      const medicationsData = await response.json(); // Parse response JSON
+
+      const markedDatesData = {};
+      medicationsData.forEach((item) => {
+        markedDatesData[item.selectedDate] = {
+          marked: true,
+          selected: true,
+          dotColor: "blue",
         };
       });
 
-      const date = newData.map((item) => item.selectedDate);
+      // Update state with formatted medications data
+      setMedications(medicationsData);
+      setMarkedDates(markedDatesData);
 
-      const uniqueDates = [...new Set(date)];
+      // Process medications data as needed
+      const formattedMedications = medicationsData.map((item) => ({
+        selectedDate: formatDate(item.selectedDate),
+        medicalDetails: item.medicalDetails,
+      }));
 
-      const newItems = uniqueDates.map((date) => {
-        // const specialDate=[];
-        const data = newData.filter((item) => date === item.selectedDate);
-        //  {
-        //     if (date === item.selectedDate) {
-        //         return item.medicalDetails
-        //         // specialDate.push({name:item.medicalDetails})
-        //     }
-        // });
-
-        return {
-          [date]: data.map((item) => {
-            return { name: item.medicalDetails };
-          }),
-        };
-      });
-
-      const medicationsObject = newItems.reduce((acc, curr) => {
-        const date = Object.keys(curr)[0];
-        acc[date] = curr[date];
-        return acc;
-      }, {});
-
-      // Log the transformed object
-
-      // setItems(medicationsObject);
-
-      setMedications(medicationsObject);
-      // console.log(newItems);
-
-      // console.log(newData);
+      // Update state with formatted medications data
+      setMedications(formattedMedications);
     } catch (error) {
       console.error("Error fetching medications:", error);
+      // Handle error (e.g., display error message)
     }
   };
 
   useEffect(() => {
     fetchMedications();
   }, []);
-  //////////////////////////
 
-  // Function to handle saving medication
+  const handleDayPress = (day) => {
+    console.log("Day pressed:", day);
+    setSelectedDate(day.dateString);
+  };
+
+  // const handleAddMedication = async () => {
+  //   try {
+  //     const response = await axios.post(
+  //       "http://192.168.8.104:8011/medications/add",
+  //       {
+  //         patientId,
+  //         selectedDate,
+  //         medicalDetails,
+  //       }
+  //     );
+  //     console.log(response.data); // Assuming you want to log the response
+  //     // After successfully adding medication, fetch updated medication data
+  //     fetchMedications();
+  //   } catch (error) {
+  //     console.error("Error adding medication:", error);
+  //     // Handle error
+  //   }
+  // };
+
   const saveMedication = async () => {
     try {
       // Make HTTP POST request to save medication
       console.log(
         "selectedDate:" + selectedDate + " medicalDetails:" + medicalDetails
       );
-      const res = await fetch("http://192.168.75.160:8011/medications/add", {
+      const res = await fetch("http://192.168.8.104:8011/medications/add", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -125,192 +142,151 @@ const MedicationsScreen = () => {
       if (!res.ok) {
         throw new Error("Something went wrong");
       }
-
-      //   const response = await axios.post('http://192.168.10.160:8011/medications/add', {
-      //     patientId: '214100', // Replace with actual patient ID
-      //     selectedDate: selectedDate,
-      //     medicalDetails: medicalDetails
-      //   });
-      //   console.log('Medication saved successfully:', response.data);
-      // Close modal after saving medication
       setModalVisible(false);
+      setMedicationsModalVisible(false);
     } catch (error) {
       console.error("Error saving medication:", error);
     }
   };
-
-  // const convertMedicationsToItems = (medications) => {
-  //   const items = {};
-  //   for (const date in medications) {
-  //     items[date] = medications[date].map((item) => ({ name: item.name }));
-  //   }
-  //   return items;
-  // };
-
-  // useEffect(() => {
-  //   setItems(convertMedicationsToItems(medications));
-  // }, [medications]);
-
-  //////////////////////////
-
-  // Example of items data structure
-  // const initialItems = {
-  //   "2024-03-01": [{ name: "ruchith" }],
-  //   "2024-03-12": [{ name: "cough" }, { name: "breath" }, { name: "dwdwd" }],
-  //   "2024-03-03": [{ name: "breath" }],
-  //   "2024-03-24": [], // Add an empty array for the date "2024-03-03"
-  // };
-
-  // const initialItems = {
-  //   "2024-03-01": [{ name: "ruchith" }],
-  //   // "2024-03-01": [{ name: "ruchith" }, { name: "Item 1 - Any Description" }],
-  //   "2024-03-02": [{ name: "cough" }, { name: "breath" }, { name: "dwdwd" }],
-  //   // "2024-03-13": [{ name: "Item 2 - Any Description" }],
-  //   // "2024-03-03": [],
-  // };
-  // Function to load items for a specific day
-  // const loadItems = (day) => {
-  //   const selectedMonth = day.dateString.substring(0, 7); // Extracting year-month format
-  //   // Fetch items for the selected month from the server or local storage
-  //   // For now, using initialItems as an example
-  //   setItems(medications);
-
-  //   console.log("medications", medications);
-  //   // setItems(initialItems);
-  // };
-
-  const loadItems = (day) => {
-    setTimeout(() => {
-      // for (let i = -15; i < 85; i++) {
-      //   const time = day.timestamp + i * 24 * 60 * 60 * 1000;
-      //   const strTime = timeToString(time);
-
-      //   if (!items[strTime]) {
-      //     items[strTime] = [];
-
-      //     const numItems = Math.floor(Math.random() * 3 + 1);
-      //     for (let j = 0; j < numItems; j++) {
-      //       items[strTime].push({
-      //         name: "Item for " + strTime + " #" + j,
-      //         height: Math.max(10, Math.floor(Math.random() * 150)),
-      //         day: strTime,
-      //       });
-      //     }
-      //   }
-      // }
-      // const newItems = {};
-      // Object.keys(items).forEach((key) => {
-      //   newItems[key] = items[key];
-      // });
-      setItems(medications);
-    }, 1000);
-  };
-  const timeToString = (time) => {
-    const date = new Date(time);
-    return date.toISOString().split("T")[0];
-  };
-
-  //   // Function to handle date press
   const handleDatePress = (day) => {
     setSelectedDate(day.dateString);
-    setModalVisible(true);
-  };
-
-  // Function to add event
-  const addEvent = () => {
-    if (selectedDate && patientId) {
-      // Check if patientId is available
-      const newEvent = { date: selectedDate, name: eventText };
-      setNewlyAddedEvents([...newlyAddedEvents, newEvent]);
-      const newItems = {
-        ...items,
-        [selectedDate]: [...(items[selectedDate] || []), { name: eventText }],
-      };
-      setItems(newItems);
-      // Send the medication data to the backend
+    const medicationsForDay = medications.filter(
+      (medication) => medication.selectedDate === day.dateString
+    );
+    if (medicationsForDay.length > 0) {
+      setMedicationsForSelectedDay(medicationsForDay);
+      setMedicationsModalVisible(true);
+    } else {
+      setModalVisible(true);
     }
-    setEventText("");
-    setModalVisible(false);
   };
 
-  // const renderItem = ({ item }) => {
-  //   return (
-  //     <View style={{ marginVertical: 5, backgroundColor: "" }}>
-  //       <View style={styles.tile}>
-  //         <Text>Date: {item.selectedDate}</Text>
-  //         <Text>Medical Details: {item.medicalDetails}</Text>
-  //       </View>
-  //     </View>
-  //   );
-  // };
+  const today = new Date().toISOString().split("T")[0];
 
-  const renderItem = (item, firstItemInDay) => {
+  const renderMedicationsModal = () => {
     return (
-      <View style={{ marginVertical: 5, backgroundColor: "" }}>
-        {firstItemInDay && (
-          <Text style={{ fontWeight: "bold", marginVertical: 5 }}>
-            {item.timestamp} {/* Display date */}
-          </Text>
-        )}
-        <View style={styles.tile}>
-          <Text>{item.name}</Text>
+      <Modal
+        visible={medicationsModalVisible}
+        transparent={true}
+        animationType="slide"
+        onRequestClose={() => setMedicationsModalVisible(false)}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text>Medications for {selectedDate}:</Text>
+            {medicationsForSelectedDay.map((medication, index) => (
+              <Text key={`${medication.selectedDate}-${index}`}>
+                {medication.medicalDetails}
+              </Text>
+            ))}
+
+            <Text>Add Medication</Text>
+            <TextInput
+              placeholder="Selected Date"
+              value={selectedDate}
+              onChangeText={setSelectedDate}
+              style={styles.input}
+            />
+            <TextInput
+              placeholder="Medical Details"
+              value={medicalDetails}
+              onChangeText={setMedicalDetails}
+              style={[styles.input, { height: 100 }]}
+              multiline
+            />
+
+            <Button
+              title="Close"
+              onPress={() => setMedicationsModalVisible(false)}
+            />
+            <Button title="Save" onPress={saveMedication} />
+          </View>
         </View>
-      </View>
+      </Modal>
     );
   };
 
   return (
     <View style={{ flex: 1 }}>
       <Header2 text="Medications" />
-      <View style={{ flex: 1 }}>
-        <Agenda
-          items={items}
-          loadItemsForMonth={loadItems}
-          selected={"2024-03-01"}
-          onDayPress={handleDatePress}
-          renderItem={renderItem}
-        />
-        {/* <FlatList
-          data={medications}
-          keyExtractor={(item) => item.selectedDate}
-          renderItem={renderItem}
-        /> */}
 
-        <Modal
-          visible={modalVisible}
-          transparent={true}
-          animationType="slide"
-          onRequestClose={() => setModalVisible(false)}
-        >
-          <View style={styles.modalContainer}>
-            <View style={styles.modalContent}>
-              <Text>Add Medication</Text>
-              <TextInput
-                placeholder="Selected Date"
-                value={selectedDate}
-                onChangeText={setSelectedDate}
-                style={styles.input}
-              />
-              <TextInput
-                placeholder="Medical Details"
-                value={medicalDetails}
-                onChangeText={setMedicalDetails}
-                style={[styles.input, { height: 100 }]}
-                multiline
-              />
-              <View style={styles.buttonContainer}>
-                <Button title="Cancel" onPress={() => setModalVisible(false)} />
-                <Button title="Save" onPress={saveMedication} />
-              </View>
+      <Calendar
+        markedDates={markedDates}
+        dayComponent={({ date, state }) => {
+          return (
+            <View>
+              <TouchableOpacity
+                style={{
+                  backgroundColor:
+                    date.dateString === today ? "#D0F1FF" : "white",
+                  height: 70,
+                  width: 50,
+                  borderRadius: 15,
+                  padding: 10,
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+                onPress={() => handleDatePress(date)}
+              >
+                <Text
+                  style={{ color: state === "disabled" ? "gray" : "black" }}
+                >
+                  {date.day}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          );
+        }}
+      />
+      {renderMedicationsModal()}
+
+      <View>
+        <Text>Medications:</Text>
+        {medications && medications.length > 0 ? (
+          medications.map((medication, index) => (
+            <Text key={`${medication.selectedDate}-${index}`}>
+              {medication.selectedDate}
+              {medication.medicalDetails}
+            </Text>
+          ))
+        ) : (
+          <Text>No medications found</Text>
+        )}
+      </View>
+      <Modal
+        visible={modalVisible}
+        transparent={true}
+        animationType="slide"
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text>Add Medication</Text>
+            <TextInput
+              placeholder="Selected Date"
+              value={selectedDate}
+              onChangeText={setSelectedDate}
+              style={styles.input}
+            />
+            <TextInput
+              placeholder="Medical Details"
+              value={medicalDetails}
+              onChangeText={setMedicalDetails}
+              style={[styles.input, { height: 100 }]}
+              multiline
+            />
+            <View style={styles.buttonContainer}>
+              <Button title="Cancel" onPress={() => setModalVisible(false)} />
+              <Button title="Save" onPress={saveMedication} />
             </View>
           </View>
-        </Modal>
-      </View>
+        </View>
+      </Modal>
     </View>
   );
 };
 
-export default MedicationsScreen;
+export default MedicationScreen;
 
 const styles = StyleSheet.create({
   modalContainer: {
@@ -333,31 +309,5 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-around",
     marginTop: 10,
-  },
-
-  tile: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    height: "auto",
-    marginRight: "3%",
-    padding: "5%",
-    borderRadius: 10,
-    backgroundColor: "#fff",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-  },
-  eventContainer: {
-    backgroundColor: "#f0f0f0",
-    padding: 10,
-    marginBottom: 5,
-    borderRadius: 5,
   },
 });
--- src/screens/ViewPatientSummaryHome Screen/PatientSummaryScreen/PatientsScreen.js
+++ src/screens/ViewPatientSummaryHome Screen/PatientSummaryScreen/PatientsScreen.js
@@ -1,16 +1,13 @@
-import {FlatList,View,ScrollView,Text} from 'react-native';
-import { LIST} from '../Data/dummy-data';
-import PatientGridTile from '../Components/PatientGridTile';
-import Search from '../Components/Search';
-import CustomHeader from '../Components/CustomHeader'
-import axios from 'axios';
-import { useState,useEffect} from 'react';
+import { FlatList, View, ScrollView, Text } from "react-native";
+import { LIST } from "../Data/dummy-data";
+import PatientGridTile from "../Components/PatientGridTile";
+import Search from "../Components/Search";
+import CustomHeader from "../Components/CustomHeader";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
-
-
-
-function PatientsScreen({navigation}){
-    const [patients, setPatients] = useState([]);
+function PatientsScreen({ navigation }) {
+  const [patients, setPatients] = useState([]);
 
   useEffect(() => {
     fetchPatients();
@@ -18,52 +15,47 @@ function PatientsScreen({navigation}){
 
   const fetchPatients = async () => {
     try {
-      const response = await axios.get('http://192.168.75.160:8011/patients');
+      const response = await axios.get("http://192.168.8.104:8011/patients");
       setPatients(response.data);
     } catch (error) {
-      console.error('Error fetching patients:', error);
+      console.error("Error fetching patients:", error);
     }
   };
 
-   
-   
-    function renderCategoryItem({item}){
-        function presshandler(){
-            navigation.navigate('PatientProfileScreen',{ptid:item._id});
+  function renderCategoryItem({ item }) {
+    function presshandler() {
+      navigation.navigate("PatientProfileScreen", { ptid: item._id });
+    }
 
-        }
-    
-        return(
-        <View>
-           {/* {patients.map(patient => (
+    return (
+      <View>
+        {/* {patients.map(patient => (
         <PatientGridTile id={patient.patientId} title={patient.title} color={patient.color} age={patient.age} gender={patient.gender} imageUrl={patient.imageUrl} onPress={presshandler}/>
       ))} */}
 
-         
-            <PatientGridTile id={item.patientId} title={item.title} color={item.color} age={item.age} gender={item.gender} imageUrl={item.imageUrl} onPress={presshandler}/>
-            </View>
-        );
-    }
-    return(
-        <View style={{ flex: 1 }}>
-            <CustomHeader />
-           
-              
-            <FlatList
-            
-            data={patients}
-            keyExtractor={(item) => item._id}
-          
-            renderItem={renderCategoryItem}
-            style={{ flex: 1 }}
-            
+        <PatientGridTile
+          id={item.patientId}
+          title={item.title}
+          color={item.color}
+          age={item.age}
+          gender={item.gender}
+          imageUrl={item.imageUrl}
+          onPress={presshandler}
         />
-                    
-           
-
-        </View>
+      </View>
     );
+  }
+  return (
+    <View style={{ flex: 1 }}>
+      <CustomHeader />
 
+      <FlatList
+        data={patients}
+        keyExtractor={(item) => item._id}
+        renderItem={renderCategoryItem}
+        style={{ flex: 1 }}
+      />
+    </View>
+  );
 }
 export default PatientsScreen;
-

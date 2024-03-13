2b34918929f1e8b23ae3b4f65c4f8f6b52d6f1ed
Povindu Samarasekara <povinduchanmith@gmail.com>
2024-03-13 11:22:47 +0530

    Select doc changes

--- src/screens/GiveDocAccess/SelectDocForAccessScreen.js
+++ src/screens/GiveDocAccess/SelectDocForAccessScreen.js
@@ -19,27 +19,28 @@ const baseUrl = 'http://10.0.2.2:4000/api';
 function SelectDocForAccess({ navigation }) {
 
   const [DocList,setDocList] = useState([])
+  const fetchDoctors = async () => {
+    try{
+      const configurationObject = {
+        method: 'get',
+        url: `${baseUrl}/doctors`,
+      };
+      console.log(configurationObject.url)
+      const response = await axios(configurationObject);
+      // console.log(response.data);
+      setDocList(response.data)
+    }
+    catch(error){
+      console.log("error " + error)
+    }
+    
+  }
+
+  
+}
 
   useEffect(() => {
-    const fetchDoctors = async () => {
-      try{
-        const configurationObject = {
-          method: 'get',
-          url: `${baseUrl}/doctors`,
-        };
-        console.log(configurationObject.url)
-        const response = await axios(configurationObject);
-        // console.log(response.data);
-        setDocList(response.data)
-      }
-      catch(error){
-        console.log("error " + error)
-      }
-      
-    }
-
-    fetchDoctors()
-  }, [])
+    fetchDoctors()}, [])
 
   const renderItem = ({ item }) => (
     <DocCardComp navigation={navigation} name={item.name} DocID={item.doctorID} id={item._id}  />

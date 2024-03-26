33f94d44652dd8bdf608f2365600fd5dd354adb7
Sanduni Lakshika <110313255+slakshika2026@users.noreply.github.com>
2024-03-26 15:43:48 +0530

    feat: connected database for test modal

---
+++ src/screens/AddMedicalRecord/MedicalRecordScreen.js
@@ -0,0 +1,12 @@
+import { Text, StyleSheet, View, SafeAreaView, TouchableOpacity, Dimensions } from 'react-native';
+// import Header2 from '../AddMedicalIncidentScreen/components/Header2';
+import Header from '../../components/Header';
+
+const MedicalRecordScreen=()=>{
+    return(
+        <>
+        <Header name="New Medical Record" />
+        </>  
+    )
+}
+export default MedicalRecordScreen;

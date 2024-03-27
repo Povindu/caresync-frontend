29f79cea62342e6a34bf3605b71b0fb7d13b241a
Povindu Samarasekara <povinduchanmith@gmail.com>
2024-03-27 16:43:31 +0530

    fix: patientDashboard style fix

--- src/screens/DashboardScreens/PatientDashboard.js
+++ src/screens/DashboardScreens/PatientDashboard.js
@@ -1,11 +1,18 @@
 import React from "react";
-import { View, Text, Image, TouchableOpacity, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+  ScrollView,
+} from "react-native";
 const PatientDashboard = ({ navigation }) => {
   const navigateToMedicalHistory = () => {
-    navigation.navigate('MedicalHistory');
+    navigation.navigate("MedicalHistory");
   };
   return (
-    <>
+    <ScrollView>
       <View style={styles.container}>
         <View style={styles.topPanel}>
           <Text style={styles.titleMain}>CareSync</Text>
@@ -26,101 +33,68 @@ const PatientDashboard = ({ navigation }) => {
         </View>
 
         <View style={styles.dashboardContainer}>
-
           <View style={styles.row}>
-            <TouchableOpacity style={styles.dashboardButton} onPress={navigateToMedicalHistory}>
+            <TouchableOpacity
+              style={styles.dashboardButton}
+              onPress={navigateToMedicalHistory}
+            >
               <Image
                 source={require("../../../assets/DocImage.png")}
                 style={styles.dashboardImage}
-
               />
-              <Text style={styles.dashboardButtonText}>View Medical History</Text>
+              <Text style={styles.dashboardButtonText}>
+                View Medical History
+              </Text>
             </TouchableOpacity>
 
-            <TouchableOpacity style={styles.dashboardButton} onPress={() => { navigation.navigate('TestSelection') }}>
+            <TouchableOpacity
+              style={styles.dashboardButton}
+              onPress={() => {
+                navigation.navigate("TestSelection");
+              }}
+            >
               <Image
                 source={require("../../../assets/DocImage.png")}
                 style={styles.dashboardImage}
               />
-              <Text style={styles.dashboardButtonText}>Perform Medical Tests</Text>
-            </TouchableOpacity></View>
-
-            <View style={styles.row}>
-            <TouchableOpacity style={styles.dashboardButton} onPress={() => { navigation.navigate('MedicationView') }}>
-              <Image
-                source={require("../../../assets/DocImage.png")}
-                style={styles.dashboardImage}
-              />
-              <Text style={styles.dashboardButtonText}>View Medication Plans</Text>
+              <Text style={styles.dashboardButtonText}>
+                Perform Medical Tests
+              </Text>
             </TouchableOpacity>
-
-            <TouchableOpacity style={styles.dashboardButton} onPress={() => { navigation.navigate('MedicalIncidentHomeScreen') }}>
-              <Image
-                source={require("../../../assets/DocImage.png")}
-                style={styles.dashboardImage}
-              />
-              <Text style={styles.dashboardButtonText}>Add Medical Incident</Text>
-            </TouchableOpacity>
-            </View>
-
-            <View style={styles.row}>
-            <TouchableOpacity style={styles.dashboardButton} onPress={() => { navigation.navigate('NewMedicalRecordScreen') }}>
-              <Image
-                source={require("../../../assets/DocImage.png")}
-                style={styles.dashboardImage}
-              />
-              <Text style={styles.dashboardButtonText}>Add Medical Record</Text>
-            </TouchableOpacity></View>
-
           </View>
 
-          <TouchableOpacity style={styles.roundedPlusButton}>
-            <Text style={styles.plusButtonText}>+</Text>
-          </TouchableOpacity>
+          <View style={styles.row}>
+            <TouchableOpacity
+              style={styles.dashboardButton}
+              onPress={() => {
+                navigation.navigate("MedicationView");
+              }}
+            >
+              <Image
+                source={require("../../../assets/DocImage.png")}
+                style={styles.dashboardImage}
+              />
+              <Text style={styles.dashboardButtonText}>
+                View Medication Plans
+              </Text>
+            </TouchableOpacity>
 
+            <TouchableOpacity
+              style={styles.dashboardButton}
+              onPress={() => {
+                navigation.navigate("MedicalIncidentHomeScreen");
+              }}
+            >
+              <Image
+                source={require("../../../assets/DocImage.png")}
+                style={styles.dashboardImage}
+              />
+              <Text style={styles.dashboardButtonText}>
+                Add Medical Incident
+              </Text>
+            </TouchableOpacity>
+          </View>
 
-          <TouchableOpacity
-            style={styles.dashboardButton}
-            onPress={() => {
-              navigation.navigate("MedicationView");
-            }}
-          >
-            <Image
-              source={require("../../../assets/DocImage.png")}
-              style={styles.dashboardImage}
-            />
-            <Text style={styles.dashboardButtonText}>
-              View Medication Plans
-            </Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            style={styles.dashboardButton}
-            onPress={() => {
-              navigation.navigate("MedicalIncidentHomeScreen");
-            }}
-          >
-            <Image
-              source={require("../../../assets/DocImage.png")}
-              style={styles.dashboardImage}
-            />
-            <Text style={styles.dashboardButtonText}>Add Medical Incident</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            style={styles.dashboardButton}
-            onPress={() => {
-              navigation.navigate("SelectDocForAccessScreen");
-            }}
-          >
-            <Image
-              source={require("../../../assets/DocImage.png")}
-              style={styles.dashboardImage}
-            />
-            <Text style={styles.dashboardButtonText}>
-              Give Access to Doctors
-            </Text>
-          </TouchableOpacity>
           <TouchableOpacity
             style={styles.dashboardButton}
             onPress={() => {
@@ -133,111 +107,112 @@ const PatientDashboard = ({ navigation }) => {
               source={require("../../../assets/DocImage.png")}
               style={styles.dashboardImage}
             />
-            <Text style={styles.dashboardButtonText}>
-              LogOut
-            </Text>
+            <Text style={styles.dashboardButtonText}>LogOut</Text>
           </TouchableOpacity>
         </View>
 
-      </>
-      );
+        <TouchableOpacity style={styles.roundedPlusButton}>
+          <Text style={styles.plusButtonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
+  );
 };
 
-      const styles = StyleSheet.create({
-        container: {
-        flex: 1,
-      backgroundColor: "#F7FEFF",
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F7FEFF",
   },
-      topPanel: {
-        flexDirection: "row",
-      justifyContent: "space-between",
-      alignItems: "center",
-      height: 95,
-      backgroundColor: "#00567D",
-      paddingHorizontal: 15,
-  },
-      titleMain: {
-        fontSize: 24,
-      color: "#fff",
-      fontWeight: "bold",
-  },
-      profileButton: {
-        width: 40,
-      height: 40,
-      left: 120,
-      borderRadius: 20,
-      overflow: "hidden",
-      backgroundColor: "#D9D9D9",
-  },
-      profileImageContainer: {
-        width: "100%",
-      height: "100%",
-      justifyContent: "center",
-      alignItems: "center",
-  },
-      profileImage: {
-        width: "80%",
-      height: "80%",
-  },
-      bellIcon: {
-        width: 40,
-      height: 40,
-      right: 45,
-      top: 3,
-  },
-      bellImage: {
-        width: "60%",
-      height: "90%",
-  },
-      dashboardContainer: {
-        flexDirection: "column",
-      justifyContent: "space-between",
-      marginTop: 30,
-      paddingHorizontal: 15,
-  },
-      dashboardButton: {
-        width: 163,
-      height: 163,
-      backgroundColor: "#D9F8FF",
-      borderRadius: 15,
-      marginBottom: 20,
-      alignItems: "center",
-      justifyContent: "center",
-  },
-      dashboardImage: {
-        width: "80%",
-      height: "70%",
-      resizeMode: "contain",
-      backgroundColor: "#fff",
-  },
-      dashboardButtonText: {
-        marginTop: 10,
-      fontSize: 14,
-      fontWeight: "bold",
-      textAlign: "center",
-      color: "#2c3e50",
-  },
-      roundedPlusButton: {
-        position: "absolute",
-      bottom: 50,
-      right: 20,
-      width: 60,
-      height: 60,
-      backgroundColor: "#3498db",
-      borderRadius: 30,
-      alignItems: "center",
-      justifyContent: "center",
-  },
-      plusButtonText: {
-        fontSize: 40,
-      color: "#fff",
-      fontWeight: "bold",
-  },
-  row:{
-    flexDirection:"row",
+  topPanel: {
+    flexDirection: "row",
     justifyContent: "space-between",
-
-  }
+    alignItems: "center",
+    height: 95,
+    backgroundColor: "#00567D",
+    paddingHorizontal: 15,
+  },
+  titleMain: {
+    fontSize: 24,
+    color: "#fff",
+    fontWeight: "bold",
+  },
+  profileButton: {
+    width: 40,
+    height: 40,
+    left: 120,
+    borderRadius: 20,
+    overflow: "hidden",
+    backgroundColor: "#D9D9D9",
+  },
+  profileImageContainer: {
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  profileImage: {
+    width: "80%",
+    height: "80%",
+  },
+  bellIcon: {
+    width: 40,
+    height: 40,
+    right: 45,
+    top: 3,
+  },
+  bellImage: {
+    width: "60%",
+    height: "90%",
+  },
+  dashboardContainer: {
+    flexDirection: "column",
+    justifyContent: "space-between",
+    marginTop: 30,
+    paddingHorizontal: 15,
+  },
+  dashboardButton: {
+    width: 163,
+    height: 163,
+    backgroundColor: "#D9F8FF",
+    borderRadius: 15,
+    marginBottom: 20,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  dashboardImage: {
+    width: "80%",
+    height: "70%",
+    resizeMode: "contain",
+    backgroundColor: "#fff",
+  },
+  dashboardButtonText: {
+    marginTop: 10,
+    fontSize: 14,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#2c3e50",
+  },
+  roundedPlusButton: {
+    position: "absolute",
+    bottom: 50,
+    right: 20,
+    width: 60,
+    height: 60,
+    backgroundColor: "#3498db",
+    borderRadius: 30,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  plusButtonText: {
+    fontSize: 40,
+    color: "#fff",
+    fontWeight: "bold",
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
 });
 
-      export default PatientDashboard;
+export default PatientDashboard;
